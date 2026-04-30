b057d20cb1f9240579db26cc8065b298c588b24e
Prem MIshra <premmishrasonb@gmail.com>
2026-04-30 22:54:15 +0530

    fix

--- backend/server.js
+++ backend/server.js
@@ -5,21 +5,30 @@ import connectDB from "./config/db.js";
 import contactRoutes from "./routes/contactRoutes.js";
 
 dotenv.config();
-
 connectDB();
 
 const app = express();
 
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://prem-portfolio-zst6.vercel.app",
+];
+
 app.use(
   cors({
-    origin: [
-      "http://localhost:5173",
-      "https://prem-portfolio-zst6.vercel.app",
-    ],
-    methods: ["GET", "POST"],
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    methods: ["GET", "POST", "OPTIONS"],
     credentials: true,
   })
 );
+
+app.options("*", cors()); // important
 
 app.use(express.json());
 
@@ -32,5 +41,5 @@ app.get("/", (req, res) => {
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
