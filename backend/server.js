b37bf5b5925aaf173b537a2af6b1a47139fdcebd
Prem MIshra <premmishrasonb@gmail.com>
2026-04-30 22:37:32 +0530

    change

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,36 @@
- import express from 'express';
- import cors from 'cors';
- import dotenv from "dotenv";
- import connectDB from "./config/db.js";
- import contactRoutes from './routes/contactRoutes.js';
-  dotenv.config();
-  connectDB();
-  const app = express();
-  app.use(
+ import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+import contactRoutes from "./routes/contactRoutes.js";
+
+dotenv.config();
+
+connectDB();
+
+const app = express();
+
+app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://prem-portfolio-zst6.vercel.app"
+      "https://prem-portfolio-zst6.vercel.app",
     ],
-    methods: ["GET", "POST"]
+    methods: ["GET", "POST"],
+    credentials: true,
   })
 );
-  app.use(express.json());
-  app.use("/api/contact", contactRoutes);
-  const PORT = process.env.PORT||5000;
-  app.listen(PORT,()=>{
-    console.log("server is running on the port"+PORT);
-  });
+
+app.use(express.json());
+
+app.use("/api/contacts", contactRoutes);
+
+app.get("/", (req, res) => {
+  res.send("Backend Running");
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`server running on port ${PORT}`);
+});
